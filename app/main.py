4cdd36e34cda8a59d639af43085e7939f4cc199d
Jay <rangeatom1379@gmail.com>
2026-08-18 13:09:08 +0530

    Synthetic data and basic FastApi integration

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI
-from backend.database.connection import connection
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+from backend.database.connection  import Base, engine, get_db
+from sqlalchemy import text
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
+"""
+@app.get("/")
+def check_connection(db: Session = Depends(get_db)):
+  return {"message": "Database session successfully established!"}
+"""
 
 
 @app.get("/")
-async def root():
-    with connection as conn:
-        with conn.cursor() as cur:
-            cur.execute("SELECT * FROM categories")
-            categories = cur.fetchall()
+def get_users_raw(db: Session = Depends(get_db)):
+  rows = db.execute(text("SELECT * FROM categories")).mappings().all()
+  return [dict(row) for row in rows]
